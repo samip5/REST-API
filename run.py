3687e498abe69ada0e46c5cdf63ccccd50222787
samip5 <sm@samip.fi>
2019-02-08 09:19:23 +0200

    Sovellusken polun päivitystä.

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ def create_app(config_filename):
     app.config.from_object(config_filename)
 
     from app import api_bp
-    app.register_blueprint(api_bp, url_prefix='/api')
+    app.register_blueprint(api_bp, url_prefix='/')
 
     from models import db
     db.init_app(app)
@@ -17,4 +17,4 @@ app = create_app("config")
 
 if __name__ == "__main__":
     #app = create_app("config")
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=False)
