bcd50d61ef03ec5fba385116ac077f0b5b3cbc40
samip5 <sm@samip.fi>
2019-03-09 02:04:21 +0200

    API kirjautumisen uudelleen kirjoitus: JWT initialization.

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -2,12 +2,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
 
 from .config import config_by_name
 
 db = SQLAlchemy()
 ma = Marshmallow()
 flask_bcrypt = Bcrypt()
+jwt = JWTManager()
 
 
 def create_app(config_name):
@@ -15,5 +17,6 @@ def create_app(config_name):
     app.config.from_object(config_by_name[config_name])
     db.init_app(app)
     flask_bcrypt.init_app(app)
+    jwt.init_app(app)
 
     return app
